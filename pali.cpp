abe171106847e7f693b5595ff6955dc27d50d62d
XyLiGan4eg2005 <kostikkostik2049@icloud.com>
2024-03-17 18:40:46 +0700

    Русский язык и др.

--- pali.cpp
+++ pali.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <string>
+#include <Windows.h>
 using namespace std;
 
-int isPali(std::string str) {
+//Функция проверки строки на палиндром
+int isPali(string str) {
+	//Сравнение i-ых элементов с начала и с конца строки
 	for (int i = 0; i < str.length() / 2; i++) {
 		if (tolower(str[i]) != tolower(str[str.length() - i - 1])) {
 			return 0;
@@ -11,6 +14,18 @@ int isPali(std::string str) {
 	return 1;
 }
 
-int main(){
-    cout << isPali("heh");
+int main() {
+	//Русский язык
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	//Ввод строки
+	cout << "Введите строку" << endl;
+	string str = "";
+	getline(cin, str);
+	//Вывод результата проверки
+	if (isPali(str)) {
+		cout << "Строка является палиндромом";
+	} else {
+		cout << "Строка не является палиндромом";
+	}
 }
