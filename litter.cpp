2831cc2fa7b7b71dbe7f619f75dc451cb380cb54
Ruslana-Po <potapovaruslana7@gmail.com>
2024-03-17 19:48:48 +0700

    Поправки

--- litter.cpp
+++ litter.cpp
@@ -1,13 +1,13 @@
-#include<string> 
+﻿﻿#include<string>
 #include <iostream> 
 #include <Windows.h> 
 using namespace std;
 int vowelCount(string str) {
     int kolCount = 0;
-    // ‘¯¨á®ª £« á­ëå
-    string lettersLower = { 'a','e','u','o','i','y',' ','¥','ñ','¨','ã','®','í','î','ï','ë' };
-    string lettersUp = { '€','…','ð','ˆ','“','Ž','','ž','Ÿ','›','A','E','U','O','I','Y' }; 
-    // –¨ª« ¯®¨áª  £« á­ëå
+    // Список гласных
+    string lettersLower = { 'a','e','u','o','i','y','а','е','ё','и','у','о','э','ю','я','ы' };
+    string lettersUp = { 'А','Е','Ё','И','У','О','Э','Ю','Я','Ы','A','E','U','O','I','Y' };
+    // Цикл поиска гласных
     for (int i = 0; i < str.length(); i++) {
         for (int j = 0; j < lettersUp.length(); j++) {
             if (str[i] == lettersLower[j] || str[i] == lettersUp[j]) {
@@ -20,14 +20,14 @@ int vowelCount(string str) {
 }
 int main()
 {
-    // ®¤ª«îç¥­¨¥ àãááª®£® ï§ëª 
+    // Подключение русского языка
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     string str = "";
-    cout << "‚¢¥¤¨â¥ áâà®ªy: " << endl;
-    // ‚¢®¤ áâà®ª¨ 
+    cout << "Введите строкy: " << endl;
+    // Ввод строки 
     getline(cin, str);
     int kolCount = vowelCount(str);
-    cout << "‚ áâà®ª¥ " << kolCount << " £« á­ëå ¡ãª¢";
+    cout << "В строке " << kolCount << " гласных букв";
     return 0;
 }
